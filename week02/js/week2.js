c608aa0a9c9c2f26527949c92c222562b51de6fc
Dallin Gilbert <djgilb24@byui.edu>
2021-10-16 21:48:04 -0600

    fixing js files for quiz ninja examples

--- week02/js/week2.js
+++ week02/js/week2.js
@@ -1,3 +1,3 @@
-const question = "What is Superman's real name?"
-const answer = prompt(question);
-alert(`You answered ${answer}`);
+// const question = "What is Superman's real name?"
+// const answer = prompt(question);
+// alert(`You answered ${answer}`);
--- week05/js/week5.js
+++ week05/js/week5.js
@@ -1,38 +1,65 @@
-const quiz = [
-    { name: "Superman",realName: "Clark Kent" },
-    { name: "Wonder Woman",realName: "Diana Prince" },
-    { name: "Batman",realName: "Bruce Wayne" },
-];
+// const quiz = [
+//     { name: "Superman",realName: "Clark Kent" },
+//     { name: "Wonder Woman",realName: "Diana Prince" },
+//     { name: "Batman",realName: "Bruce Wayne" },
+// ];
 
-const game = {
-    start(quiz){
-        this.questions = [...quiz];
-        this.score = 0;
-        // main game loop
-        for(const question of this.questions){
-        this.question = question;
-        this.ask();
-        }
-        // end of main game loop
-        this.gameOver();
-    },
-    ask(){
-        const question = `What is ${this.question.name}'s real name?`;
-        const response =  prompt(question);
-        this.check(response);
-    },
-    check(response){
-        const answer = this.question.realName;
-        if(response === answer){
-        alert('Correct!');
-        this.score++;
+// const game = {
+//     start(quiz){
+//         this.questions = [...quiz];
+//         this.score = 0;
+//         // main game loop
+//         for(const question of this.questions){
+//         this.question = question;
+//         this.ask();
+//         }
+//         // end of main game loop
+//         this.gameOver();
+//     },
+//     ask(){
+//         const question = `What is ${this.question.name}'s real name?`;
+//         const response =  prompt(question);
+//         this.check(response);
+//     },
+//     check(response){
+//         const answer = this.question.realName;
+//         if(response === answer){
+//         alert('Correct!');
+//         this.score++;
+//         } else {
+//         alert(`Wrong! The correct answer was ${answer}`);
+//         }
+//     },
+//     gameOver(){
+//         alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+//     }
+// }
+
+function amIOldEnough(age){
+    console.log(age);
+        if (age < 12) {
+        console.log(`In the if with ${age}`);
+        return 'No, sorry.';
+        } else if (age < 18) {
+        console.log(`In the else-if with ${age}`);
+        return 'Only if you are accompanied by an adult.';
         } else {
-        alert(`Wrong! The correct answer was ${answer}`);
-        }
-    },
-    gameOver(){
-        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        console.log(`In the else with ${age}`);
+        return 'Yep, come on in!';
     }
 }
 
-game.start(quiz);
+function imaginarySquareRoot(number) {
+    'use strict';
+    let answer;
+    try {
+        answer = String(squareRoot(number));
+    } catch(error) {
+        answer = squareRoot(-number)+"i";
+    } finally {
+        return `+ or - ${answer}`;
+    }
+}
+
+amIOldEnough(15);
+console.log(imaginarySquareRoot(4));
